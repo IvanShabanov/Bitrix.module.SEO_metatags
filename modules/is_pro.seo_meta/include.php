2f7c10cf1f08550636991e36b923d539eed9ebb8
Иван Шабанов <coder10@web-axioma.ru>
2025-12-01 11:33:08 +0700

    chore: remove debug

--- modules/is_pro.seo_meta/include.php
+++ modules/is_pro.seo_meta/include.php
@@ -11,9 +11,3 @@ $arClasses = [
 ];
 
 \Bitrix\Main\Loader::registerAutoLoadClasses($arModuleCfg['MODULE_ID'], $arClasses);
-
-if (class_exists('IS_PRO\SEO_metatags\MainFunctions')) {
-    file_put_contents(__DIR__ . '/log.log', 'IS_PRO\SEO_metatags\MainFunctions class' . "\n", FILE_APPEND);
-} else {
-    file_put_contents(__DIR__ . '/log.log', '----' . "\n", FILE_APPEND);
-}
